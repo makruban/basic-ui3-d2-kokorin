b7c1d7dda1d99c5d9208f436174884c793500c04
makruban <co1@ukr.net>
2021-06-18 18:38:56 +0300

    untill bottom

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,9 @@ class MyApp extends StatelessWidget{
       title: "UI Task_2",
       theme: ThemeData(
         brightness: Brightness.dark,
-        primaryColor: Color(0xFFFF5336),
-        scaffoldBackgroundColor: Color(0xFFFF5336),
+        primaryColor: Color(0xFFFF5232),
+        scaffoldBackgroundColor: Color(0xFFFF5232),
+        // backgroundColor: Color(0xFFFF5336),
       ),
       home: MyHomePage(),
     );
@@ -23,10 +24,11 @@ class MyHomePage extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         centerTitle: true,
         title: Text('Weather Forecast',
         style: TextStyle(
-          fontSize: 20.0,
+          fontSize: 22.0,
           color: Colors.white,
         ),),
       ),
@@ -41,27 +43,150 @@ class MyBodyPage extends StatelessWidget{
     return SingleChildScrollView(
       child: Column(
         children: [
-          SearchLine(),
+          TextField(),
+          CityDetail(),
+          TemperatureDetail(),
+          ExtraWeatherDetail(),
         ],
       ),
     );
   }
 }
 
-class SearchLine extends StatelessWidget{
+class TextField extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(10.0),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Icon(Icons.search, size: 30.0,),
-          SizedBox(),
+          Icon(Icons.search, size: 28.0,),
+          SizedBox(
+            width: 12.0,
+          ),
          Text('Entry City Name', style: TextStyle(
-           fontSize: 19.0,
+           fontSize: 17.0,
          ),
          ) ,
+        ],
+      ),
+    );
+  }
+}
+
+class CityDetail extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30.0),
+      child: Column(
+        children: [
+          Text("Kharkiv, UA",
+              style:  TextStyle(
+                fontSize: 40.0,
+                fontFamily: 'Raleway',
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text('Friday, June 18, 2021',
+          style: TextStyle(
+            fontSize: 19.0,
+            fontFamily: 'Raleway',
+            fontWeight: FontWeight.bold,
+          ),)
+        ],
+      ),
+    );
+  }
+}
+
+class TemperatureDetail extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Padding (
+      padding: EdgeInsets.only(top: 60.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.wb_sunny,
+          size: 80.0,),
+          SizedBox(
+            width: 20.0,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('26 °С',
+              style: TextStyle(
+                fontFamily: 'Raleway',
+                // fontWeight: FontWeight.bold,
+                fontSize: 61.0,
+              ),),
+
+              Text('SUNNY',
+                style: TextStyle(
+                  fontFamily: 'Raleway',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17.0,
+                ),),
+            ],
+          ),
+
+        ],
+      ),
+    );
+  }
+}
+
+class ExtraWeatherDetail extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+       return Padding(
+      padding: EdgeInsets.only(top: 50.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            children: [
+              Icon(Icons.arrow_forward_sharp,
+              size: 35.0,),
+              Text('2',
+              style: TextStyle(
+                fontSize: 21.0,
+
+              ),),
+              Text('km/hr'),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.wb_sunny_rounded,
+                size: 35.0,),
+              Text('98',
+              style: TextStyle(
+                fontSize: 21.0,
+
+              ),),
+              Text('%'),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.wb_cloudy_outlined,
+                size: 35.0,),
+              Text('2',
+              style: TextStyle(
+                fontSize: 21.0,
+
+              ),),
+              Text('%'),
+            ],
+          ),
         ],
       ),
     );
